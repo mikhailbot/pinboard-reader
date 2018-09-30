7cf7d839dd619f7a1465d345fa31b60065c4fdee
Mikhail Delport <mikhail@delport.ca>
2018-09-29 20:55:15 -0400

    Remove proxy requirements

--- mix.exs
+++ mix.exs
@@ -39,7 +39,6 @@ defmodule PinboardReader.Mixfile do
       {:httpoison, "~> 1.3.1", override: true},
       {:readability, "~> 0.9"},
       {:sbroker, "~> 1.0-beta"},
-      {:terraform, "~> 1.0.1"},
       {:cors_plug, "~> 1.5"},
       {:gproc, "~> 0.6.1"},
     ]
